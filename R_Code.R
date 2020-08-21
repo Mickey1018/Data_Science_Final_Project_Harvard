12862f7bcfc2343fdfcb233f59aa8e38fbf9c1b9
Mickey1018 <MIckeyfung18@gmail.com>
2020-08-21 14:05:12 +0800

    change to seismic data

--- R_Code.R
+++ R_Code.R
@@ -2,22 +2,23 @@ if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 if(!require(readr)) install.packages("readr", repos = "http://cran.us.r-project.org")
+if(!require(dslabs)) install.packages("dslabs", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-project.org")
 
 library(tidyverse)
 library(caret)
 library(data.table)
 library(readr)
+library(dslabs)
+library(caret)
+library(lubridate)
 
-#Read csv data first, and then change them into dataframe
+#Read csv data first, and then change them into data frame
+seismic <- as.data.frame(read_csv("data/seismic_bumps.csv"))
+summary(seismic)
 
-covid_jpn_prefecture <- as.data.frame(read_csv("data/covid_jpn_prefecture.csv"))
-covid_jpn_metadata <- as.data.frame(read_csv("data/covid_jpn_metadata.csv"))
-covid_jpn_total <- as.data.frame(read_csv("data/covid_jpn_total.csv"))
-
-head(covid_jpn_metadata)
-head(covid_jpn_prefecture)
-head(covid_jpn_total)
-
-
-
-
+#Analysis on covid_jpn_prefecture data set
+covid_jpn_prefecture %>% ggplot(aes(Prefecture,Positive)) +  
+  geom_bar(width = 0.5, stat = "identity", color = "blue") + 
+  coord_flip()
